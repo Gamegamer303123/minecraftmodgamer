32dd8d2d41b5b593ebe8cf954ed0f4ef68e40e67
Gamegamer303123 <106033325+Gamegamer303123@users.noreply.github.com>
2024-09-11 17:39:48 +0200

    Update ModItems.java

--- src/main/java/net/gamegamer/tutorialmod/item/ModItems.java
+++ src/main/java/net/gamegamer/tutorialmod/item/ModItems.java
@@ -2,6 +2,7 @@ package net.gamegamer.tutorialmod.item;
 
 import net.gamegamer.tutorialmod.TutorialMod;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Rarity;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -12,7 +13,10 @@ public class ModItems {
             DeferredRegister.create(ForgeRegistries.ITEMS, TutorialMod.MOD_ID);
 
     public static final RegistryObject<Item> TIMETOTEM = ITEMS.register("time_totem",
-            () -> new Item(new Item.Properties()));
+            () -> new Item(new Item.Properties().stacksTo(1).rarity(Rarity.RARE)));
+
+    public static final RegistryObject<Item> EBUSPEN = ITEMS.register("ebus_pen",
+            () -> new EbusPenItem(new Item.Properties().stacksTo(1).rarity(Rarity.EPIC)));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
